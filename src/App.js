37c17e0a85660e11d83b50f9a4d9ce502bb4d9d0
Takashi Idobe <idobetakashi@gmail.com>
2018-07-02 19:12:04 -0500

    changed to work with openweathermap api and adding 5day forecast functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 //use the freecodecamp api to render weather
 class App extends Component {
@@ -11,115 +12,98 @@ class App extends Component {
       //what we'll set from getLocation() call
       longitude: undefined,
       latitude: undefined,
-      tempScale: undefined,
-      //the raw stamps to check against sunrise/sunset
-      timestamp: undefined,
-      sunriseStamp: undefined,
-      sunsetStamp: undefined,
-      //the final Date objects
-      temp: undefined,
-      fahrenheitTemp: undefined,
-      time: undefined,
-      sunrise: undefined,
-      sunset: undefined,
+      tempScale: "fahrenheit",
+      weather: [],
+      // After the openweathermap call
+      city: '',
+      country: '',
+      humidity: '',
+
+
       //show error message if this is true
       error: undefined
     };
 
     //bind the methods so we can use them
-    this.getLocation = this.getLocation.bind(this);
-    this.getTemp = this.getTemp.bind(this);
     this.changeTempScale = this.changeTempScale.bind(this);
+    this.getTemp = this.getTemp.bind(this);
+    this.logState = this.logState.bind(this);
   }
 
-  //uses the navigator api to find location
-  //if the user says no, set this.state.error = true
-  getLocation() {
-    navigator.geolocation.getCurrentPosition (
-      (position) => {
-        this.setState({
-          latitude: position.coords.latitude.toFixed(2),
-          longitude: position.coords.longitude.toFixed(2),
-          error: null
-        });
-      },
-      (error) => this.setState({ error: true }),
-      //makes the function call high accuracy for 20 seconds at max
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 10000 },
-    )
+  componentDidMount() {
+    navigator.geolocation
+      .getCurrentPosition(
+        position => {
+          this.setState({
+            latitude: position.coords.latitude.toFixed(2),
+            longitude: position.coords.longitude.toFixed(2),
+            error: null
+          });
+        },
+        error => this.setState({ error: true }),
+        //makes the function call high accuracy for 20 seconds at max
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 10000 }
+      )
+  }
+
+  getTemp() {
+      axios
+        .get("http://api.openweathermap.org/data/2.5/forecast", {
+          params: {
+            APPID: "ce8af8b27335a58a4a597637fbdf7ec8",
+            lat: `${this.state.latitude}`,
+            lon: `${this.state.longitude}`
+          }
+        })
+        .then(res => {
+          console.log(res.data);
+          this.setState({
+            weather: res.data.list
+          })
+
+        }).catch(err => {
+          console.log(err);
+        })
+  }
+
+  logState() {
+    console.log(this.state.weather);
   }
 
   //change from fahrenheit to celcius to kelvin
   changeTempScale() {
-    this.state.tempScale === 'fahrenheit' ? this.setState({ tempScale: 'celcius' }) : this.setState({ tempScale: 'fahrenheit' });
+    this.state.tempScale === "fahrenheit"
+      ? this.setState({ tempScale: "celcius" })
+      : this.setState({ tempScale: "fahrenheit" });
   }
-
-  //gets the temp off the coords provided
-  getTemp() {
-    //Get requesting the api
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://fcc-weather-api.glitch.me/api/current?lat=${this.state.latitude}&lon=${this.state.longitude}`, true);
-
-      request.onload = () => {
-        //On a successful call
-        if (request.status >= 200 && request.status < 400) {
-          //parses data from json to javascript
-          const data = JSON.parse(request.responseText);
-
-          //set variables which will take in the call data
-          let temp =  (data.main.temp).toFixed(2);
-          let time = new Date(data.dt * 1000).toLocaleString();
-          let sunrise = new Date(data.sys.sunrise * 1000).toLocaleString();
-          let sunset = new Date(data.sys.sunset * 1000).toLocaleString();
-          
-          //set state call 
-          this.setState({
-            temp: temp,
-            fahrenheitTemp: temp * 1.8 + 32,
-            time: time,
-            sunrise: sunrise,
-            sunset: sunset,
-            //turns timestamps into proper unix time
-            timestamp: data.dt * 1000,
-            sunriseStamp: data.sys.sunrise * 1000,
-            sunsetStamp: data.sys.sunset * 1000
-          });
-            
-        } else {
-          //error call
-          this.setState({ error: true })
-        }
-      };
-
-      request.onerror = () => {
-        // There was a connection error of some sort
-        this.setState({ error: true })
-      }
-      request.send();
-  }
-
-  
 
   render() {
     return (
       <div className="App">
-        <h3>Weather App with the FCC API</h3>
-        <p>Press the button to get your weather</p>
-        <button onClick={this.getLocation}>Press Me to get your longitude and latitude</button>
-        <button onClick={this.getTemp}>Press Me to get your weather!</button>
-        <button onClick={this.changeTempScale}>Change Temp Scale</button>
-        <div>Your temp scale is currently: {this.state.tempScale}</div>
-        <div id="output">
-          {this.state.error && `Sorry, we couldn't get your location or weather`}
-          {this.state.latitude && `Your Latitude is: ${this.state.latitude}`} 
-          <br />
-          {this.state.longitude && `Your Longitude is: ${this.state.longitude}`} 
-          <br />
-          {this.state.tempScale === 'fahrenheit' ? `The temperature outside is: ${this.state.fahrenheitTemp} degrees` : `The temperature outside is:  ${this.state.temp} degrees`}
-          <br />
-          {this.state.timestamp && `The sunrise ${(this.state.timestamp > this.state.sunriseStamp ? 'will be' : 'was')} at: ${this.state.sunrise}`}
-          <br />
-          {this.state.timestamp && `The sunset ${(this.state.timestamp < this.state.sunsetStamp ? 'will be' : 'was')} at: ${this.state.sunset}`}
+        <div className="container">
+          <h3>Five day Weather Forecast</h3>
+          <p>Press the button to get your weather</p>
+          <button onClick={this.getTemp}>Press Me to get your weather!</button>
+          <button onClick={this.changeTempScale}>Change Temp Scale</button>
+          <button onClick={this.logState}>Log State with this button</button>
+          <div>Your temp scale is currently: {this.state.tempScale}</div>
+          <div id="output">
+            {this.state.error &&
+              `Sorry, we couldn't get your location or weather`}
+            {this.state.latitude && `Your Latitude is: ${this.state.latitude}`}
+            <br />
+            {this.state.longitude &&
+              `Your Longitude is: ${this.state.longitude}`}
+            <br />
+
+            <ul>
+              {
+                this.state.weather.map((item, i) => {
+                  <li key={i}>{item}</li>
+                })
+              }
+            </ul>
+          </div>
         </div>
       </div>
     );
@@ -127,4 +111,3 @@ class App extends Component {
 }
 
 export default App;
-
